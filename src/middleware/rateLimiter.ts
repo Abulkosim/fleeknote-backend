59fd75a5242360e5f718b1db4b6ac08f0cc700e4
Abulkosim <abulkasimkattaev@gmail.com>
2025-08-21 00:40:55 +0500

    update: increase rate limits

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -3,7 +3,7 @@ import { createError } from '../utils/errors';
 
 export const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, 
-    max: 500, 
+    max: 5000, 
     message: 'Too many requests from this IP, please try again after 15 minutes',
     handler: (req, res, next, options) => {
         next(createError(429, options.message));
@@ -14,7 +14,7 @@ export const apiLimiter = rateLimit({
 
 export const authLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, 
-    max: 10, 
+    max: 50, 
     message: 'Too many login attempts, please try again after an hour',
     handler: (req, res, next, options) => {
         next(createError(429, options.message));
